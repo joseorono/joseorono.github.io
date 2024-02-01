3b54344557798f5cedd740f65a86e61bbd2143b0
José Miguel Oroño <joseomaker@gmail.com>
2024-02-01 02:20:07 -0500

    Add Tailwind starting theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,21 @@ export default {
 	theme: {
 		extend: {},
 	},
+	
 	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+		  {
+			dark: {
+				...require("daisyui/src/theming/themes")["dark"],
+				"primary": "#38bdf8",
+				"secondary": "#818CF8",
+				"accent": "#F471B5",
+				"success": "#2DD4BF",
+				"warning": "#F4BF50",
+				"error": "#FB7085",
+			},
+		  },
+		],
+	  },
 }
