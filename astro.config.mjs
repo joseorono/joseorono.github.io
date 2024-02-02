59bc5f9c000737700d9487b12040266a6333b534
José Miguel Oroño <joseomaker@gmail.com>
2024-02-02 04:01:24 -0500

    General Update: Social Icons and cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    // By default, all icons from every installed set are included!
+    // You can use the `include` option to include only the icons you need.
+    // https://www.astroicon.dev/reference/configuration#include
+    icon({
+      include: {
+        mdi: ['linkedin', 'gmail', 'github'],
+      }
+    })
+  ]
 });
